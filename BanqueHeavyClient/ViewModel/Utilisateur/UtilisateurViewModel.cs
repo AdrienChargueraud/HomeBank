d421d64edd19e3967a59e300865966d0fab5761e
adrien <adrien.chargueraud@gmail.com>
2013-12-13 13:14:18 +0100

    Début du référentiel utilisateur

--- BanqueHeavyClient/ViewModel/Utilisateur/UtilisateurViewModel.cs
+++ BanqueHeavyClient/ViewModel/Utilisateur/UtilisateurViewModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using BanqueLogicLayer.Modele;
 using BanqueHeavyClient.Global;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace BanqueHeavyClient.ViewModel.Utilisateur
 {
@@ -12,6 +14,14 @@ namespace BanqueHeavyClient.ViewModel.Utilisateur
         #region Propriétés
 
         private BanqueContainer RefContexte { get; set; }
+        public ObservableCollection<DetailUtilisateurViewModel> UserReferentiel { get; set; }
+
+        private DetailUtilisateurViewModel _userSelect;
+        public DetailUtilisateurViewModel UserSelect
+        {
+            get { return _userSelect; }
+            set { _userSelect = value; OnPropertyChanged("IsBoutonActif"); }
+        }
 
         #endregion
 
@@ -20,8 +30,61 @@ namespace BanqueHeavyClient.ViewModel.Utilisateur
         public UtilisateurViewModel()
         {
             RefContexte = SessionUtilisateur.Instance.banqueContexte;
+            ExtractAllUsers();
         }
 
         #endregion
+
+        #region Binding
+        public Visibility IsBoutonActif
+        {
+            get
+            {
+                return ((UserSelect != null) ? Visibility.Visible : Visibility.Hidden);
+            }
+        }
+        #endregion
+
+        #region Metier
+        public void ExtractAllUsers()
+        {
+            UserReferentiel = new ObservableCollection<DetailUtilisateurViewModel>();
+
+            OnPropertyChanged("UserReferentiel");
+        }
+
+        private void ShowWindowOperation(int mode)
+        {
+            //View.OrganismeUpdate view = new View.OrganismeUpdate();
+            //view.DataContext = new UpdateOrganismeViewModel(this, mode);
+            //view.ShowDialog();
+        }
+
+        private void DeleteUser()
+        {
+            var transaction = RefContexte.Database.BeginTransaction();
+            try
+            {
+               
+                RefContexte.SaveChanges();
+                transaction.Commit();
+
+                ExtractAllUsers();
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+                new Erreur("Impossible de supprimer l'organisme sélectionné!", "Organisme", Enums.TypeErreurMessage.ERREUR).DisplayErreur();
+            }
+            finally
+            {
+                transaction.Dispose();
+            }
+
+            if (UserReferentiel.Count > 0)
+                UserSelect = UserReferentiel.ElementAt(0);
+        }
+        #endregion
+
     }
 }
